28e397c6b6c8ba63ddc726981e74ac14ed2e5828
Phú Phán <karamata@gmail.com>
2021-04-08 15:31:39 +0700

    update

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const doCache = false;
+
+const CACHE_NAME = 'brt_cloud_platform';
+
+console.log('========');
+
+self.addEventListener('activate', event => {
+});
+
+self.addEventListener('error', event => {
+});
+
+self.addEventListener('install', function(event) {
+});
+
+self.addEventListener('fetch', function(event) {
+});
