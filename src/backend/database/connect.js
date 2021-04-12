3af3698aa15274e85f6004698b5e8d96a712801e
huy.pham@inapps.net <huy.pham@inapps.net>
2021-04-12 13:51:17 +0700

    fix(task): connect mongodb

--- src/backend/database/connect.js
+++ src/backend/database/connect.js
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose';
 
 export default function connect() {
-  return mongoose.connect(process.env.MONGODB_URL || 'mongodb://db:27017/todoapp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
+  return mongoose
+    .connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/todoapp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    .then(res => {
+      console.log('MongoDB connected');
+    })
+    .catch(err => {
+      console.log('MongoDB connect failed');
+    });
 }
